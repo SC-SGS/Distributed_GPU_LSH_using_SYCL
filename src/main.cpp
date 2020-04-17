1935da804c7fae16d840cefdd10d35683066cb85
Marcel Breyer <breyerml@studi.informatik.uni-stuttgart.de>
2020-04-17 22:25:50 +0200

    added main file

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file src/main.cpp
+ * @author Marcel Breyer
+ * @date 2020-04-17
+ */
+
+#include <iostream>
+
+#include <dummy/dummy.hpp>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << lsh::add(1, 2) << std::endl;
+    return 0;
+}
