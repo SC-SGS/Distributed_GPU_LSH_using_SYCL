24c2db318517176a0d4ccdc11c4f05ea75ab05ab
Marcel Breyer <breyerml@studi.informatik.uni-stuttgart.de>
2020-04-17 22:25:33 +0200

    added dummy header and source file

---
+++ include/dummy/dummy.hpp
@@ -0,0 +1,16 @@
+/**
+ * @file include/dummy/dummy.hpp
+ * @author Marcel Breyer
+ * @date 2020-04-17
+ */
+
+#ifndef DISTRIBUTED_GPU_LSH_IMPLEMENTATION_USING_SYCL_DUMMY_HPP
+#define DISTRIBUTED_GPU_LSH_IMPLEMENTATION_USING_SYCL_DUMMY_HPP
+
+namespace lsh {
+
+    int add(const int, const int);
+
+}
+
+#endif //DISTRIBUTED_GPU_LSH_IMPLEMENTATION_USING_SYCL_DUMMY_HPP
