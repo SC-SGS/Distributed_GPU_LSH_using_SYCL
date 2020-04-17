35600083f4c715d6e4445e684007b68db591272b
Marcel Breyer <breyerml@studi.informatik.uni-stuttgart.de>
2020-04-17 22:19:02 +0200

    added config.hpp file for project global settings

---
+++ include/config.hpp
@@ -0,0 +1,14 @@
+/**
+ * @file include/config.hpp
+ * @author Marcel Breyer
+ * @date 2020-04-17
+ *
+ * @brief Contains project global configurations.
+ */
+
+#ifndef DISTRIBUTED_GPU_LSH_USING_SYCL_CONFIG_HPP
+#define DISTRIBUTED_GPU_LSH_USING_SYCL_CONFIG_HPP
+
+using real_t = double;
+
+#endif //DISTRIBUTED_GPU_LSH_USING_SYCL_CONFIG_HPP
