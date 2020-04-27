ef60cbcb256ae007cf03cc7324c3544dabc0dc57
Marcel Breyer <breyer.marcel@web.de>
2020-04-27 19:01:08 +0200

    - changed printf to custom print - added constant memory - added local memory

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@
 #include <cassert>
 #include <type_traits>
 
+#include <detail/print.hpp>
+
 namespace sycl = cl::sycl;
 
 
@@ -19,12 +21,12 @@ using real_t = float;
 using index_t = uint32_t;
 
 int main() {
-    constexpr std::size_t dim = 16;
-    constexpr std::size_t number_of_hashes = 16;
+    constexpr std::size_t dim = 32;
+    constexpr std::size_t number_of_hashes = 32;
     // w
     constexpr real_t w = 1.0;
     constexpr index_t prim = 105613;
-    constexpr std::size_t size = 1000;
+    constexpr std::size_t size = 2048 * 2048;
 
     std::vector<real_t> hash_functions(number_of_hashes * (dim + 1));
 
@@ -54,7 +56,14 @@ int main() {
     std::cout << queue.get_device().get_info<sycl::info::device::name>() << std::endl;
     std::cout << queue.get_device().get_info<sycl::info::device::vendor>() << std::endl;
 
-    auto wgroup_size = device.get_info<sycl::info::device::max_work_group_size>();
+    const auto previous_power_of_two = [&] (const auto local_mem_size) -> std::size_t {
+        return std::pow(2, std::floor(std::log2(local_mem_size / (dim * sizeof(real_t)))));
+    };
+
+    auto local_mem_size = queue.get_device().get_info<sycl::info::device::local_mem_size>();
+    const auto max_work_item_sizes = queue.get_device().get_info<sycl::info::device::max_work_item_sizes>();
+    const auto local_size = std::min(previous_power_of_two(local_mem_size), max_work_item_sizes[0]);
+    std::cout << "local size: " << local_size << std::endl;
 
     const auto start_time = std::chrono::steady_clock::now();
     {
@@ -63,48 +72,37 @@ int main() {
         sycl::buffer buf_hashes(hashes.data(), sycl::range<1>{ hashes.size() });
 
         queue.submit([&](sycl::handler& cgh) {
-            auto acc_hf = buf_hash_functions.get_access<sycl::access::mode::read>(cgh);
+            auto acc_hf = buf_hash_functions.get_access<sycl::access::mode::read, sycl::access::target::constant_buffer>(cgh);
             auto acc_d = buf_data.get_access<sycl::access::mode::read>(cgh);
             auto acc_h = buf_hashes.get_access<sycl::access::mode::discard_write>(cgh);
 
-            const std::size_t local_size = 128;
+            sycl::accessor<real_t, 1, sycl::access::mode::read_write, sycl::access::target::local> local_mem(sycl::range<1>{ local_size * dim }, cgh);
+
+
             const auto round_up = [](const auto x, const auto y) { return ((x + y - 1) / y) * y; };
             const std::size_t global_size = round_up(size, local_size);
             const std::size_t num_group = global_size / local_size;
 
-            std::cout << "local_size: " << local_size << std::endl;
-            std::cout << "global_size: " << global_size << std::endl;
-            std::cout << "num_group: " << num_group << std::endl;
-
             auto execution_range = sycl::nd_range<1>{ sycl::range<1>{ global_size }, sycl::range<1>{ local_size } };
-            std::cout << execution_range.get_global_range()[0] << " " << execution_range.get_local_range()[0] << std::endl;
 
             cgh.parallel_for<class hash_point>(execution_range, [=](sycl::nd_item<1> item_id) {
                 const std::size_t idx = item_id.get_global_linear_id();
+                const std::size_t local_idx = item_id.get_local_linear_id();
 
-                const std::size_t global_id = item_id.get_global_id(0);
-                const std::size_t local_id = item_id.get_local_id(0);
 
-                if (global_id == 0) {
-                    printf(idx == global_id ? "true\n" : "false\n");
+                if (idx >= size) { return; }
+
+                for (std::size_t i = 0; i < dim; ++i) {
+                    local_mem[i + local_idx * dim] = acc_d[i + idx * dim];
                 }
 
-
-                if (global_id == 0 || global_id == 64 || global_id == 127 || global_id == 128 || global_id == 129 || global_id == 256) {
-                    printf("%lu -> %lu (%lu) -> %lu \n", global_id, local_id, item_id.get_group(0), idx);
-
-                }
-
-                if (idx >= size) {
-                    if (idx == size) printf("Oversubscribed\n");
-                    return;
-                }
+                item_id.barrier(sycl::access::fence_space::local_space);
 
                 index_t combined_hash = number_of_hashes;
                 for (std::size_t j = 0; j < number_of_hashes; ++j) {
                     real_t hash = acc_hf[dim + j * (dim + 1)];
                     for (std::size_t d = 0; d < dim; ++d) {
-                        hash += acc_d[d + idx * dim] * acc_hf[d + j * (dim + 1)];
+                        hash += local_mem[d + local_idx * dim] * acc_hf[d + j * (dim + 1)];
                     }
                     combined_hash ^= static_cast<index_t>(hash / w) + 0x9e3779b9 + (combined_hash << 6u) + (combined_hash >> 2u);
                 }
