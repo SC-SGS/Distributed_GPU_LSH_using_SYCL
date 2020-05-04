475dd1a282bede733583fa19d4bec62b9510a0e4
Marcel Breyer <breyer.marcel@web.de>
2020-05-04 19:16:22 +0200

    added documentation

--- include/config.hpp
+++ include/config.hpp
@@ -1,16 +1,28 @@
+/**
+ * @file
+ * @author Marcel Breyer
+ * @date 2020-05-04
+ *
+ * @brief Contains global constants, usings and enums.
+ */
+
 #ifndef DISTRIBUTED_GPU_LSH_USING_SYCL_CONFIG_HPP
 #define DISTRIBUTED_GPU_LSH_USING_SYCL_CONFIG_HPP
-
 
 #include <CL/sycl.hpp>
 
 
 namespace sycl = cl::sycl;
+/// Namespace containing helper classes and functions.
+namespace detail { }
 
 
+/// The memory layout.
 enum class memory_layout {
-    aos, // Array of Structs
-    soa  // Struct of Arrays
+    /// Array of Structs
+    aos,
+    /// Struct of Arrays
+    soa
 };
 
 
