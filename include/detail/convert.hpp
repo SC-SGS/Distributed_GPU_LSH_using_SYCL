cf8eaef5518feb26c601614b3d1237014e944cab
Marcel Breyer <breyer.marcel@web.de>
2020-05-05 21:37:24 +0200

    added enable_if and documentation

--- include/detail/convert.hpp
+++ include/detail/convert.hpp
@@ -1,3 +1,11 @@
+/**
+ * @file
+ * @author Marcel Breyer
+ * @date 2020-05-05
+ *
+ * @brief Implements a conversion function from std::string to numeric types.
+ */
+
 #ifndef DISTRIBUTED_GPU_LSH_IMPLEMENTATION_USING_SYCL_CONVERT_HPP
 #define DISTRIBUTED_GPU_LSH_IMPLEMENTATION_USING_SYCL_CONVERT_HPP
 
@@ -5,11 +13,22 @@
 #include <string>
 #include <type_traits>
 
+
 namespace detail {
 
-    template <typename T>
+    /**
+     * @brief Attempt to convert the value represented by @p str to a value of type `T`.
+     * @tparam T the value type to parse @p str to
+     * @param str the string to parse
+     * @return the value of type `T` represented by @p str
+     *
+     * @throw std::invalid_argument if @p str can't get parsed to type `T`.
+     */
+    template <typename T, std::enable_if_t<std::is_arithmetic_v<T>, int> = 0>
     T convert_to(const std::string& str) {
         if constexpr (std::is_floating_point_v<std::decay_t<T>>) {
+            // try to convert str to a floating point type using the old stof/stod/stold functions
+            // since std::from_chars doesn't support floating point type syet
             try {
                 if constexpr (std::is_same_v<std::decay_t<T>, float>) {
                     return std::stof(str);
@@ -19,19 +38,21 @@ namespace detail {
                     return std::stold(str);
                 }
             } catch (const std::exception& e) {
-                throw std::invalid_argument("Can't parse string!: " + str);
+                throw std::invalid_argument("Can't parse string '" + str + "'!");
             }
         } else {
+            // convert str to integral type using std::from_chars
             T val;
             auto [p, ec] = std::from_chars(str.data(), str.data() + str.size(), val);
             if (ec == std::errc()) {
                 return val;
             } else {
-                throw std::invalid_argument("Can't parse string!: " + str);
+                throw std::invalid_argument("Can't parse string '" + str + "'!");
             }
         }
     }
 
 }
+
 
 #endif // DISTRIBUTED_GPU_LSH_IMPLEMENTATION_USING_SYCL_CONVERT_HPP
