926b91cc33ec8813e682fc2052ff942db1e738b8
Marcel Breyer <breyer.marcel@web.de>
2020-05-06 19:08:49 +0200

    changed clang warning message for get_as function

--- include/data.hpp
+++ include/data.hpp
@@ -53,7 +53,7 @@ public:
     template <memory_layout new_layout>
     [[nodiscard]] data<new_layout, Options> get_as()
     __attribute__((diagnose_if(new_layout == layout,
-            "get_as called with same memory_layout as *this -> results in a copy of *this -> better use *this in the first place",
+            "get_as called with same memory_layout as *this -> results in a copy of *this -> directly use *this",
             "warning")))
     {
         data<new_layout, Options> new_data(size, dims);
