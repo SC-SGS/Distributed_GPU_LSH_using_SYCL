ec685dd599a5a653f2a746ab61d5b7b858c5dce0
Marcel Breyer <breyer.marcel@web.de>
2020-05-07 19:27:49 +0200

    created test files (empty) for the hash_functions class

---
+++ test/hash_functions/construction.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * @author Marcel Breyer
+ * @date 2020-05-07
+ *
+ * @brief Test cases for the construction of a @ref hash_functions object.
+ * @details Testsuite: *HashFunctionsTest*
+ * | test case name | test case description |
+ * |:---------------|:----------------------|
+ */
+
+#include <gtest/gtest.h>
+
+#include <hash_function.hpp>
+
+
---
+++ test/hash_functions/copy_to_different_layout.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * @author Marcel Breyer
+ * @date 2020-05-07
+ *
+ * @brief Test cases for the @ref hash_functions::get_as() member function.
+ * @details Testsuite: *HashFunctionsTest*
+ * | test case name | test case description |
+ * |:---------------|:----------------------|
+ */
+
+#include <gtest/gtest.h>
+
+#include <hash_function.hpp>
+
+
