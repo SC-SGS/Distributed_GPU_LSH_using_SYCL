82c1f222fa14d70e53f379626132c26cde348714
Marcel Breyer <breyer.marcel@web.de>
2020-05-13 15:15:18 +0200

    added doxygen header, changed make_hash_tables function call

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,11 @@
+/**
+ * @file
+ * @author Marcel Breyer
+ * @date 2020-05-13
+ *
+ * @brief The main file containing the main logic.
+ */
+
 #include <iostream>
 
 #include <config.hpp>
@@ -16,7 +24,7 @@ int main() {
         auto data = make_data<memory_layout::aos>(opt, 10, 3);
 //        auto data = make_data<memory_layout::aos>(opt, "../data_sets/DR5_nowarnings_less05_test.txt");
         auto hash_functions = make_hash_functions<memory_layout::aos>(data);
-        auto hash_tables = make_hash_tables<memory_layout::aos>(queue, data);
+        auto hash_tables = make_hash_tables(queue, hash_functions);
     } catch (const std::exception& e) {
         std::cerr << e.what() << std::endl;
     }
