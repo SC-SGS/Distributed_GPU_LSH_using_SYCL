d6ec7ac079e97ab38cb3ea2553598f320ceb9df8
Marcel Breyer <breyer.marcel@web.de>
2020-06-26 16:05:42 +0200

    revised file parser

--- include/file_parser/arff_parser.hpp
+++ include/file_parser/arff_parser.hpp
@@ -3,28 +3,18 @@
  * @author Marcel Breyer
  * @date 2020-06-18
  *
- * @brief File parser for parsing plain data files.
+ * @brief File parser for parsing `.arff`` data files.
  */
 
 
 #ifndef DISTRIBUTED_GPU_LSH_IMPLEMENTATION_USING_SYCL_ARFF_PARSER_HPP
 #define DISTRIBUTED_GPU_LSH_IMPLEMENTATION_USING_SYCL_ARFF_PARSER_HPP
 
-
-#include <algorithm>
-#include <iostream>
-#include <fstream>
-#include <limits>
 #include <stdexcept>
-#include <string>
-
-#include <mpi.h>
 
 #include <config.hpp>
-#include <detail/convert.hpp>
-#include <file_parser/base_file_parser.hpp>
-#include <detail/mpi_type.hpp>
-#include <mpi_buffer.hpp>
+#include <file_parser/base_parser.hpp>
+#include <options.hpp>
 
 
 /**
@@ -48,12 +38,12 @@
  *
  * @note The file **must** be saved in **binary** form.
  */
-template <memory_layout layout, typename Options>
-class arff_parser final : public file_parser<layout, Options> {
+template <typename Options>
+class arff_parser final : public file_parser<Options> {
     static_assert(std::is_base_of_v<detail::options_base, Options>, "The second template parameter must by a 'options' type!");
 
     /// The type of the base @ref file_parser.
-    using base = file_parser<layout, Options>;
+    using base = file_parser<Options>;
 public:
     /// The type of the underlying data as specified as in the provided @ref options class.
     using real_type = typename Options::real_type;
@@ -61,25 +51,21 @@ public:
     using index_type = typename Options::index_type;
 
     /**
-     * @brief Constructs a new @ref default_parser object for parsing plain data files.
-     * @param[in] file the file to parse
+     * @brief Constructs a new @ref arff_parser object for parsing `.arff` data files.
+     * @param[in] file_name the file to parse
      * @param[in] communicator the *MPI_Comm* communicator used to open the @p file with
-     * @param[in] comm_rank the current MPI rank
      *
      * @throw std::invalid_argument if @p file doesn't exist
      * @throw std::logic_error **always** since reading text files isn't supported (yet)
      */
-    arff_parser(const std::string& file, const MPI_Comm& communicator, const int comm_rank)
-        : file_parser<layout, Options>(file, communicator, comm_rank)
-    {
-//        detail::mpi_print<print_rank>(communicator, "Parsing an '.arff' file using the default_parser together with MPI IO!\n");
+    arff_parser(const std::string& file_name, const MPI_Comm& communicator) : file_parser<Options>(file_name, communicator) {
         throw std::logic_error("Parsing an '.arff' file is currently no supported! Maybe run data_sets/convert_arff_to_binary.py first?");
     }
 
-    [[nodiscard]] index_type parse_size() const override { throw std::logic_error("Not implemented yet!"); }
+    [[nodiscard]] index_type parse_total_size() const override { throw std::logic_error("Not implemented yet!"); }
     [[nodiscard]] index_type parse_rank_size() const override { throw std::logic_error("Not implemented yet!"); }
     [[nodiscard]] index_type parse_dims() const override { throw std::logic_error("Not implemented yet!"); }
-    [[nodiscard]] mpi_buffers<real_type, index_type> parse_content() const override { throw std::logic_error("Not implemented yet!"); }
+    void parse_content(real_type*) const override { throw std::logic_error("Not implemented yet!"); }
 
 };
 
