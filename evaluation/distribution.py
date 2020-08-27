b3cf2f57c0c077640e4d83ddca27a332da071eb6
Marcel Breyer <breyer.marcel@web.de>
2020-08-27 14:01:06 +0200

    added plot output for presentation

--- evaluation/distribution.py
+++ evaluation/distribution.py
@@ -1,5 +1,5 @@
 # @author Marcel Breyer
-# @date 2020-07-29
+# @date 2020-08-21
 # @brief Python3 script for printing the hash table bucket distribution.
 
 
@@ -26,9 +26,26 @@ nrows = math.ceil(data.shape[0] / ncols)
 arr = []
 
 for idx in range(data.shape[0]):
-    arr.append([idx, sum(1 for val in data[idx] if val == 0), max(data[idx]), statistics.median(data[idx]), statistics.mode(data[idx])])
-    plt.subplot(nrows, ncols, idx + 1)
-    plt.plot(data[idx])
+    arr.append([idx, sum(1 for val in data[idx] if val == 0), max(data[idx]), statistics.mean(data[idx]), \
+                statistics.median(data[idx]), statistics.mode(data[idx]), np.var(data[idx])])
+    # plt.subplot(nrows, ncols, idx + 1)
+    # plt.plot(data[idx])
 
-print(tabulate.tabulate(arr, headers=["hash_table", "zeros", "max", "median", "mode"], tablefmt="orgtbl"))
+print(tabulate.tabulate(arr, headers=["hash_table", "zeros", "max", "mean", "median", "mode", "variance"], tablefmt="orgtbl"))
+
+
+# distribution plot for slides
+fig, ax = plt.subplots()
+ax.set_title("Random Projections", fontsize=26)
+ttl = ax.title
+ttl.set_position([.5, 1.05])
+ax.set_xlabel("hash value", fontsize=22)
+ax.set_ylabel("occurrence", fontsize=22)
+ax.tick_params(labelsize=20)
+ax.grid()
+plt.plot(data[0])
+# fig.set_size_inches(16, 9)
+fig.savefig("random_projections_distribution.png", bbox_inches="tight")
+
+
 plt.show()
