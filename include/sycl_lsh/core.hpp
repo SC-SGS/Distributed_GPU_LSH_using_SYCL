fbdd5f502acd8c3605a447e85d7101cabbddcc9b
Marcel Breyer <breyer.marcel@web.de>
2020-09-18 18:05:28 +0200

    added core header including all other necessary headers

---
+++ include/sycl_lsh/core.hpp
@@ -0,0 +1,23 @@
+/**
+ * @file
+ * @author Marcel Breyer
+ * @date 2020-09-18
+ *
+ * @brief Core header which includes every other necessary header file, i.e. \c \#include` <sycl_lsh/core.hpp>` is sufficient to use every
+ *        important function or class.
+ */
+#ifndef DISTRIBUTED_GPU_LSH_IMPLEMENTATION_USING_SYCL_CORE_HPP
+#define DISTRIBUTED_GPU_LSH_IMPLEMENTATION_USING_SYCL_CORE_HPP
+
+// include all necessary library headers
+#include <sycl_lsh/mpi/communicator.hpp>
+#include <sycl_lsh/mpi/errhandler.hpp>
+#include <sycl_lsh/mpi/main.hpp>
+
+
+/// The main namespace. Nearly all functions are located in this namespace.
+namespace sycl_lsh { }
+/// This namespace is for implementation details only and **should not** be used directly bey users.
+namespace sycl_lsh::detail { }
+
+#endif // DISTRIBUTED_GPU_LSH_IMPLEMENTATION_USING_SYCL_CORE_HPP
