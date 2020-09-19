9cf89c620d89fe94060f3ef2f9a2e0bb5f08f46d
Marcel Breyer <breyer.marcel@web.de>
2020-09-19 16:12:05 +0200

    wrapped in try/catch block

--- src/main.cpp
+++ src/main.cpp
@@ -21,17 +21,25 @@ int custom_main(int argc, char** argv) {
     sycl_lsh::errhandler handler(sycl_lsh::errhandler::type::comm);
     comm.attach_errhandler(handler);
 
-    // parse command line arguments
-    sycl_lsh::argv_parser parser(argc, argv);
-    // print help message if requested
-    if (parser.has_argv("help")) {
+    try {
+        
+        // parse command line arguments
+        sycl_lsh::argv_parser parser(argc, argv);
+        // print help message if requested
+        if (parser.has_argv("help")) {
+            if (comm.rank() == 0) {
+                std::cout << parser.description() << std::endl;
+                return EXIT_SUCCESS;
+            }
+        }
+
+    } catch (const std::exception& e) {
         if (comm.rank() == 0) {
-            std::cout << parser.description() << std::endl;
-            return EXIT_SUCCESS;
+            std::cout << e.what() << std::endl;
         }
     }
     
-    return 0;
+    return EXIT_SUCCESS;
 }
 
 
