2edf1ecdc29da3a84a7c389ef82aeaf5568bc880
Marcel Breyer <breyer.marcel@web.de>
2020-09-22 16:03:27 +0200

    silenced some clang-tidy warnings

--- include/sycl_lsh/mpi/logger.hpp
+++ include/sycl_lsh/mpi/logger.hpp
@@ -29,7 +29,7 @@ namespace sycl_lsh::mpi {
          * @param[in] comm the used @ref sycl_lsh::mpi::communicator
          * @param[in,out] out the output-stream to log on
          */
-        logger(const communicator& comm, std::ostream& out = std::cout);
+        explicit logger(const communicator& comm, std::ostream& out = std::cout);
 
 
         // ---------------------------------------------------------------------------------------------------------- //
@@ -40,23 +40,23 @@ namespace sycl_lsh::mpi {
          * @param[in] comm_rank the MPI rank to log on
          * @param[in] msg the message to log
          */
-        void log(const int comm_rank, const std::string_view msg);
+        void log(int comm_rank, std::string_view msg);
         /**
          * @brief Log the given message @p msg **only** on the MPI rank 0 (master rank).
          * @param[in] msg the message to log
          */
-        void log(const std::string_view msg);
+        void log(std::string_view msg);
         /**
          * @brief Log the given message @p msg **only** in the MPI rank 0 (master rank).
          * @param[in] msg the message to log
          */
-        void log_on_master(const std::string_view msg);
+        void log_on_master(std::string_view msg);
         /**
          * @brief Log the given messages @p msg on all MPI ranks.
          * @details Gathers all messages on MPI rank 0 (master rank) to enable a deterministic output.
          * @param[in] msg the message to log
          */
-        void log_on_all(const std::string_view msg);
+        void log_on_all(std::string_view msg);
 
     private:
         const communicator& comm_;
