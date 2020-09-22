be45fcae6af547adbfedd7709da41e71fccfbf0d
Marcel Breyer <breyer.marcel@web.de>
2020-09-22 16:33:06 +0200

    added description of sycl_lsh::mpi namespace

--- include/sycl_lsh/core.hpp
+++ include/sycl_lsh/core.hpp
@@ -1,7 +1,7 @@
 /**
  * @file
  * @author Marcel Breyer
- * @date 2020-09-21
+ * @date 2020-09-22
  *
  * @brief Core header which includes every other necessary header file, i.e. \c \#include` <sycl_lsh/core.hpp>` is sufficient to use every
  *        important function or class.
@@ -23,6 +23,8 @@
 
 /// The main namespace. Nearly all functions are located in this namespace.
 namespace sycl_lsh { }
+/// This namespace is for wrapper classes and functions related to MPI.
+namespace sycl_lsh::mpi { }
 /// This namespace is for implementation details only and **should not** be used directly bey users.
 namespace sycl_lsh::detail { }
 
