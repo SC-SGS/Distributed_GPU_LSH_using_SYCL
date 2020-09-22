55f5551729c65188610023400ac9472f4e0ec006
Marcel Breyer <breyer.marcel@web.de>
2020-09-22 14:16:15 +0200

    added headers for defines

---
+++ include/sycl_lsh/detail/defines.hpp
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * @author Marcel Breyer
+ * @date 2020-09-22
+ *
+ * @brief Defines named values to test against defined values (by CMake).
+ */
+
+#ifndef DISTRIBUTED_GPU_LSH_IMPLEMENTATION_USING_SYCL_DEFINES_HPP
+#define DISTRIBUTED_GPU_LSH_IMPLEMENTATION_USING_SYCL_DEFINES_HPP
+
+// defines values to test against the SYCL_LSH_TARGET
+#define SYCL_LSH_TARGET_CPU 0
+#define SYCL_LSH_TARGET_NVIDIA 1
+#define SYCL_LSH_TARGET_AMD 2
+
+#endif // DISTRIBUTED_GPU_LSH_IMPLEMENTATION_USING_SYCL_DEFINES_HPP
