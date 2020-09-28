38d68a42a7eeddc8b7acbcfb06f0c38418596381
Marcel Breyer <breyer.marcel@web.de>
2020-09-28 17:39:32 +0200

    added new empty base class for the sycl_lsh::data class

--- include/sycl_lsh/detail/defines.hpp
+++ include/sycl_lsh/detail/defines.hpp
@@ -1,7 +1,7 @@
 /**
  * @file
  * @author Marcel Breyer
- * @date 2020-09-24
+ * @date 2020-09-28
  *
  * @brief Defines helper values, classes, defines, etc. used in various places.
  */
@@ -25,6 +25,10 @@ namespace sycl_lsh::detail {
      * @brief Empty base class for the @ref sycl_lsh::options class. Only used in static_asserts.
      */
     class options_base {};
+    /**
+     * @brief Empty base class for the @ref sycl_lsh::data class. Only used in static_asserts.
+     */
+    class data_base {};
 
 }
 
