d313ff8f589750789508ea6785e6729ca3cbfa0c
Marcel Breyer <breyer.marcel@web.de>
2020-09-29 15:29:29 +0200

    updated to use data_attributes struct

--- include/sycl_lsh/data.hpp
+++ include/sycl_lsh/data.hpp
@@ -16,6 +16,7 @@
 #include <sycl_lsh/mpi/communicator.hpp>
 #include <sycl_lsh/mpi/logger.hpp>
 #include <sycl_lsh/options.hpp>
+#include <sycl_lsh/data_attributes.hpp>
 
 #include <fmt/format.h>
 #include <fmt/ostream.h>
@@ -38,71 +39,45 @@ namespace sycl_lsh {
         return data<layout, Options>(*file_parser, opt, comm, logger);
     }
 
-    template <memory_layout layout, typename index_type>
-    struct data_options {
-        static_assert(std::is_integral_v<index_type>, "The template parameter must be an integer type!");
-
-        data_options(const index_type total_size, const index_type rank_size, const index_type dims)
-            : total_size(total_size), rank_size(rank_size), dims(dims) { }
-
-        template <memory_layout other_layout>
-        data_options(const data_options<other_layout, index_type>& other)
-            : total_size(other.total_size), rank_size(other.rank_size), dims(other.dims) { }
-
-
-        const index_type total_size;
-        const index_type rank_size;
-        const index_type dims;
-    };
-
-    template <memory_layout layout, typename index_type>
-    std::ostream& operator<<(std::ostream& out, const data_options<layout, index_type>& data_opt) {
-        out << fmt::format("memory_layout '{}'\n", layout);
-        out << fmt::format("total_size {}\n", data_opt.total_size);
-        out << fmt::format("rank_size {}\n", data_opt.rank_size);
-        out << fmt::format("dims {}\n", data_opt.dims);
-        return out;
-    }
-
 
     template <memory_layout layout, typename index_type>
     [[nodiscard]]
-    constexpr index_type get_linear_id__data(const index_type point, const index_type dim, const data_options<layout, index_type>& data_opt) noexcept {
-        SYCL_LSH_DEBUG_ASSERT(0 <= point && point < data_opt.rank_size, "Out-of-bounce access for data point!\n");
-        SYCL_LSH_DEBUG_ASSERT(0 <= dim && dim < data_opt.dims, "Out-of-bounce access for dimension!\n");
+    constexpr index_type get_linear_id__data(const index_type point, const index_type dim, const data_attributes<layout, index_type>& data_attr) noexcept {
+        SYCL_LSH_DEBUG_ASSERT(0 <= point && point < data_attr.rank_size, "Out-of-bounce access for data point!\n");
+        SYCL_LSH_DEBUG_ASSERT(0 <= dim && dim < data_attr.dims, "Out-of-bounce access for dimension!\n");
 
         if constexpr (layout == memory_layout::aos) {
             // Array of Structs
-            return dim + point * data_opt.dims;
+            return dim + point * data_attr.dims;
         } else {
             // Struct of Arrays
-            return point + dim * data_opt.rank_size;
+            return point + dim * data_attr.rank_size;
         }
     }
 
 
     template <memory_layout layout, typename Options>
     class data : public detail::data_base {
+        // ---------------------------------------------------------------------------------------------------------- //
+        //                                      template parameter sanity checks                                      //
+        // ---------------------------------------------------------------------------------------------------------- //
         static_assert(std::is_base_of_v<detail::options_base, Options>, "The second template parameter must be a sycl_lsh::options type!");
     public:
         using index_type = typename Options::index_type;
         using real_type = typename Options::real_type;
         using options_type = Options;
-        using data_options_type = data_options<layout, index_type>;
+        using data_attributes_type = data_attributes<layout, index_type>;
 
         using gpu_buffer_type = sycl::buffer<real_type, 1>;
         using host_buffer_type = std::vector<real_type>;
 
-        friend std::ostream& operator<<(std::ostream& out, const data& d) {
-            return out << d.data_options_;
-        }
 
         [[nodiscard]]
         constexpr memory_layout get_memory_layout() const noexcept { return layout; }
         [[nodiscard]]
         const options_type& get_options() const noexcept { return options_; }
         [[nodiscard]]
-        const data_options_type& get_data_options() const noexcept { return data_options_; }
+        const data_attributes_type& get_data_attributes() const noexcept { return data_attributes_; }
 
         [[nodiscard]]
         gpu_buffer_type get_gpu_buffer() noexcept { return gpu_buffer_; }
@@ -115,10 +90,10 @@ namespace sycl_lsh {
 
         data(const mpi::file_parser<options_type, real_type>& parser, const options_type& opt, const mpi::communicator& comm, const mpi::logger& logger)
             : options_(opt), comm_(comm), logger_(logger),
-              data_options_(parser.parse_total_size(), parser.parse_rank_size(), parser.parse_dims()),
-              gpu_buffer_(data_options_.rank_size * data_options_.dims),
-              host_buffer_active_(data_options_.rank_size * data_options_.dims),
-              host_buffer_inactive_(data_options_.rank_size * data_options_.dims)
+              data_attributes_(parser.parse_total_size(), parser.parse_rank_size(), parser.parse_dims()),
+              gpu_buffer_(data_attributes_.rank_size * data_attributes_.dims),
+              host_buffer_active_(data_attributes_.rank_size * data_attributes_.dims),
+              host_buffer_inactive_(data_attributes_.rank_size * data_attributes_.dims)
         {
             mpi::timer t(comm_);
 
@@ -127,12 +102,12 @@ namespace sycl_lsh {
 
             // change memory layout from aos to soa if requested
             if constexpr (layout == memory_layout::soa) {
-                data_options<memory_layout::aos, index_type> parsed_data_options(data_options_);
+                data_attributes<memory_layout::aos, index_type> parsed_data_attributes(data_attributes_);
 
-                for (index_type point = 0; point < data_options_.rank_size; ++point) {
-                    for (index_type dim = 0; dim < data_options_.dims; ++dim) {
-                        host_buffer_inactive_[get_linear_id__data(point, dim, data_options_)]
-                                = host_buffer_active_[get_linear_id__data(point, dim, parsed_data_options)];
+                for (index_type point = 0; point < data_attributes_.rank_size; ++point) {
+                    for (index_type dim = 0; dim < data_attributes_.dims; ++dim) {
+                        host_buffer_inactive_[get_linear_id__data(point, dim, data_attributes_)]
+                                = host_buffer_active_[get_linear_id__data(point, dim, parsed_data_attributes)];
                     }
                 }
 
@@ -153,12 +128,18 @@ namespace sycl_lsh {
         const mpi::communicator& comm_;
         const mpi::logger& logger_;
 
-        const data_options<layout, index_type> data_options_;
+        const data_attributes_type data_attributes_;
 
         gpu_buffer_type gpu_buffer_;
         host_buffer_type host_buffer_active_;
         host_buffer_type host_buffer_inactive_;
     };
+
+
+    template <memory_layout layout, typename Options>
+    std::ostream& operator<<(std::ostream& out, const data<layout, Options>& d) {
+        return out << d.get_data_attributes();
+    }
 
 }
 
