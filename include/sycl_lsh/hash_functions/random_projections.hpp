73fe429e2ebc807e295690dc07bf01757818a9f2
Marcel Breyer <breyer.marcel@web.de>
2020-10-02 14:50:53 +0200

    changed logging function and added TODO

--- include/sycl_lsh/hash_functions/random_projections.hpp
+++ include/sycl_lsh/hash_functions/random_projections.hpp
@@ -300,6 +300,7 @@ namespace sycl_lsh {
             std::vector<real_type> hash_pool(opt.hash_pool_size * (attr.dims + 1));
             for (index_type hash_function = 0; hash_function < opt.hash_pool_size; ++hash_function) {
                 for (index_type dim = 0; dim < attr.dims; ++dim) {
+                    // TODO 2020-10-02 12:47 marcel: abs?
                     hash_pool[hash_function * (attr.dims + 1) + dim] = rnd_normal_pool_dist(rnd_normal_pool_gen);
                 }
                 hash_pool[hash_function * (attr.dims + 1) + attr.dims] = rnd_uniform_pool_dist(rnd_uniform_pool_gen);
@@ -338,7 +339,7 @@ namespace sycl_lsh {
             acc[i] = host_buffer[i];
         }
 
-        logger_.log("Created hash functions in {}.\n", t.elapsed());
+        logger_.log("Created 'random_projections' hash functions in {}.\n", t.elapsed());
     }
 
 }
