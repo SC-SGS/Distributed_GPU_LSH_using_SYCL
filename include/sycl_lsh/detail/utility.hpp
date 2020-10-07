6606fb5c562e85678fce31b935e27359a61ef16e
Marcel Breyer <breyer.marcel@web.de>
2020-10-07 17:00:04 +0200

    added swap function

--- include/sycl_lsh/detail/utility.hpp
+++ include/sycl_lsh/detail/utility.hpp
@@ -1,7 +1,7 @@
 /**
  * @file
  * @author Marcel Breyer
- * @date 2020-09-18
+ * @date 2020-10-07
  *
  * @brief Different utility functions or macros.
  */
@@ -13,5 +13,23 @@
  * @brief Shorthand macro for an easier [`std::enable_if`](https://en.cppreference.com/w/cpp/types/enable_if).
  */
 #define SYCL_LSH_REQUIRES(cond) std::enable_if_t<(cond), int> = 0
+
+namespace sycl_lsh::detail {
+
+    /**
+     * @brief Swaps the values of @p lhs and @p rhs.
+     * @tparam T the type of the elements to swap
+     * @param[in,out] lhs the first value to swap
+     * @param[in,out] rhs the second value to swap
+     */
+    template <typename T>
+    inline void swap(T& lhs, T& rhs) {
+        const T tmp = lhs;
+        lhs = rhs;
+        rhs = tmp;
+    }
+
+
+}
 
 #endif // DISTRIBUTED_GPU_LSH_IMPLEMENTATION_USING_SYCL_UTILITY_HPP
