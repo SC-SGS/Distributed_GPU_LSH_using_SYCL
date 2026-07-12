1c17fc161313e816e51ebdf568cf34792527f504
Marcel Breyer <marcel.breyer@ipvs.uni-stuttgart.de>
2026-07-12 14:16:40 +0200

    Update script to also track runtime.

--- utility_scripts/calculate_exact_nearest_neighbors.py
+++ utility_scripts/calculate_exact_nearest_neighbors.py
@@ -9,15 +9,16 @@
 import argparse
 import numpy as np
 from sklearn.neighbors import NearestNeighbors
+import time
 
 def load_dataset(path):
     with open(path, "rb") as f:
-        real_type_size = int(np.frombuffer(f.read(np.dtype(np.uint64).itemsize), dtype=np.uint64)[0])
+        real_type_size = int(np.frombuffer(f.read(np.dtype(index_type).itemsize), dtype=index_type)[0])
         parsing_type_size = int(np.dtype(real_type).itemsize)
         if real_type_size != parsing_type_size:
             raise ValueError(f"The data was stored using a {real_type_size} Byte type but is now read using a {parsing_type_size} Byte type which is not supported!")
-        num_data_points = int(np.frombuffer(f.read(np.dtype(np.uint64).itemsize), dtype=np.uint64)[0])
-        num_dimensions = int(np.frombuffer(f.read(np.dtype(np.uint64).itemsize), dtype=np.uint64)[0])
+        num_data_points = int(np.frombuffer(f.read(np.dtype(index_type).itemsize), dtype=index_type)[0])
+        num_dimensions = int(np.frombuffer(f.read(np.dtype(index_type).itemsize), dtype=index_type)[0])
         data = np.frombuffer(f.read(num_data_points * num_dimensions * parsing_type_size), dtype=real_type)
     return data.reshape(num_data_points, num_dimensions)
 
@@ -25,18 +26,18 @@ def load_dataset(path):
 def save_indices(path, indices):
     size, k = indices.shape
     with open(path, "wb") as f:
-        f.write(np.uint64(np.dtype(index_type).itemsize).tobytes())
-        f.write(np.uint64(size).tobytes())
-        f.write(np.uint64(k).tobytes())
+        f.write(index_type(np.dtype(index_type).itemsize).tobytes())
+        f.write(index_type(size).tobytes())
+        f.write(index_type(k).tobytes())
         f.write(indices.astype(index_type).tobytes())
 
 
 def save_distances(path, distances):
     size, k = distances.shape
     with open(path, "wb") as f:
-        f.write(np.uint64(np.dtype(real_type).itemsize).tobytes())
-        f.write(np.uint64(size).tobytes())
-        f.write(np.uint64(k).tobytes())
+        f.write(index_type(np.dtype(real_type).itemsize).tobytes())
+        f.write(index_type(size).tobytes())
+        f.write(index_type(k).tobytes())
         f.write(distances.astype(real_type).tobytes())
 
 
@@ -53,20 +54,22 @@ if __name__ == "__main__":
     # set the used real_type and index_type
     if args.use_64bit_real_type:
         real_type = np.float64
-        index_type = np.uint64
     else:
         real_type = np.float32
-        index_type = np.uint32
+    index_type = np.uint64
 
 
     # load the data set in the custom binary format
     points = load_dataset(args.input_file)
     print(f"Loaded {points.shape[0]} points with {points.shape[1]} dims from '{args.input_file}'")
 
-    # k+1 because results include each point itself
+    # calculate the exact k-nearest-neighbors using the brute-force algorithm
+    start = time.perf_counter()
     k_nearest_neighbors = NearestNeighbors(n_neighbors=args.knn, algorithm="brute", metric="euclidean")
     k_nearest_neighbors.fit(points)
     nn_distances, nn_ids = k_nearest_neighbors.kneighbors()
+    end = time.perf_counter()
+    print(f"k-nearest-neighbor total runtime: {end - start:.3f}s")
 
     # save the nearest-neighbor IDs
     save_indices(args.knn_indices_file, nn_ids)
